f22fab69cde12f418535e27a0fb05d275ee3dd5b
Teddy Mathabatha <60352742+TeddyDataHub@users.noreply.github.com>
2026-08-06 01:29:06 +0200

    Add Strings exercise with basic string operations

---
+++ Exercises/Strings.py
@@ -0,0 +1,15 @@
+# Strings Exercise
+
+name = "Teddy"
+course = "FNB App Academy 2026"
+
+print("Student:", name)
+print("Course:", course)
+
+message = "Learning Python is fun!"
+print(message)
+
+# String methods
+print(message.upper())
+print(message.lower())
+print(len(message))
